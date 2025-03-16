5b730454c78eba7445f554a41673ce508979ca90
ashwinr-10 <jashwinrao31@gmail.com>
2025-03-16 12:52:35 +0000

    12_List (repetitive)

--- practice_react/src/App.jsx
+++ practice_react/src/App.jsx
@@ -1,18 +1,13 @@
  
-import NetflixSeries,{Footer,Header} from "./components/NetflixSeries";
-//import {NetflixSeries} from "./components/NetflixSeries";
-//import { Footer } from "./components/NetflixSeries";
-
-
-//Default me NetflixSeries ki jagah kuch aur naam bhi de skte hain
- //Named me export aur import dono ka name match krna chahiye
+import NetflixSeries from "./components/NetflixSeries";
+ 
  export const App = () => {
-  return (<div>
-     <Header/>
+  return (
+   <> 
      <NetflixSeries/>
-     <NetflixSeries/>
-     <Footer/>
-  </div>
+  
+   </>
   );
  };
 
+ 
--- practice_react/src/components/NetflixSeries.jsx
+++ practice_react/src/components/NetflixSeries.jsx
@@ -1,48 +1,55 @@
-
-const NetflixSeries = () => {
-    const name = "Queen Of Tears";
-    const rating = "8.2";
-    let age = 18;
-    
-    const canWatch = () => {
-      if (age >= 18) return "Watch Now";
-      return "Not Available";
-    };
-  
-   
-    
-    const summary = `
-    Baek Hyun-woo and Hong Hae-in navigate 
-    a tense relationship, both at home and at work. 
-    But upon deciding his future, Hyun-woo pays a 
-    visit to his family.
-  `;
-   const getGenre = () => {
-    const genre = "Romcom";  
-    return genre;
-   };
-   
+import seriesData from "../api/seriesData.json";
+const NetflixSeries = () => { 
     return (
-           <>
-      <div>
-          <img src="qot.jpg" alt="qot.jpg" 
+      <ul>
+        <li>
+        <div>
+          <img src={seriesData[0].img_url} alt="qot.jpg" 
           width="40%" height="40%"/>
-      </div>
-      <h2>Name: {name} </h2>
-    <h3>Rating: {rating}</h3>
-    <p>Summary: {summary} </p>
-    <p>Genre: {getGenre()}</p>
-    <button>{canWatch()}</button>
-      </>
-      );
+          </div>
+          <h2>Name: {seriesData[0].name} </h2>
+        <h3>Rating: {seriesData[0].rating}</h3>
+        <p>Summary: {seriesData[0].description} </p>
+        <p>Genre: {seriesData[0].genre}</p>
+        <p>Cast: {seriesData[0].cast}</p>
+        <a href={seriesData[0].watch_url} target="_blank">
+        <button>Watch Now</button>
+        </a>
+
+        </li>
+        <li>
+        <div>
+          <img src={seriesData[1].img_url} alt="qot.jpg" 
+          width="40%" height="40%"/>
+          </div>
+          <h2>Name: {seriesData[1].name} </h2>
+        <h3>Rating: {seriesData[1].rating}</h3>
+        <p>Summary: {seriesData[1].description} </p>
+        <p>Genre: {seriesData[1].genre}</p>
+        <p>Cast: {seriesData[1].cast}</p>
+        <a href={seriesData[1].watch_url} target="_blank">
+        <button>Watch Now</button>
+        </a>
+        </li>
+
+        <li>
+        <div>
+          <img src={seriesData[2].img_url} alt="qot.jpg" 
+          width="40%" height="40%"/>
+          </div>
+          <h2>Name: {seriesData[2].name} </h2>
+        <h3>Rating: {seriesData[2].rating}</h3>
+        <p>Summary: {seriesData[2].description} </p>
+        <p>Genre: {seriesData[2].genre}</p>
+        <p>Cast: {seriesData[2].cast}</p>
+        <a href={seriesData[2].watch_url} target="_blank">
+        <button>Watch Now</button>
+        </a>
+        </li>
+      </ul>
+    );
    };
   
 export default NetflixSeries;
 
-export const Header = () => {
-    return <p>Netflix Series</p>
-}
-
-export const Footer = () => {
-    return <p>copyright @google</p>
-}
+ 
