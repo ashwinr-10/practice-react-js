37b0564c8643a60082429e57bb6edf4d5a342723
ashwinr-10 <jashwinrao31@gmail.com>
2025-04-05 00:41:15 +0000

    #26-27 React States

--- practice_react/src/App.jsx
+++ practice_react/src/App.jsx
@@ -1,22 +1,24 @@
  
-import NetflixSeries from "./components/NetflixSeries";
-import "./components/Netflix.module.css"
-import { EventHandling } from "./components/EventHandling";
-import { EventProps } from "./components/EventProps";
-import { EventPropagation } from "./components/EventPropagation";
-//import "./components/Netflix.css" 
-
+// import NetflixSeries from "./components/NetflixSeries";
+// import "./components/Netflix.module.css"
+// // import { EventHandling } from "./components/EventHandling";
+// // import { EventProps } from "./components/EventProps";
+// // import { EventPropagation } from "./components/EventPropagation";
+// //import "./components/Netflix.css" 
+import { State } from "./components/hooks/State";
  export const App = () => {
   return (
-     <section className="container">
-        <h1 className="card-heading">List of Best Netflix Series</h1>
-       <NetflixSeries/>;
-       <EventHandling/>
-       <EventProps/>
-       <EventPropagation/>
-    </section>
-
+   //   <section className="container">
+   //      <h1 className="card-heading">List of Best Netflix Series</h1>
+   //     <NetflixSeries/>;
+   //     {/* <EventHandling/> */}
+   //     {/* <EventProps/> */}
+   //     {/* <EventPropagation/> */}
+   //  </section>
+      <div classname="container">
+         <State/>
+      </div>
   );
- };
+ };  
 
  
---
+++ practice_react/src/components/hooks/State.jsx
@@ -0,0 +1,35 @@
+import {useState} from "react";
+import "../EV.css"; 
+export const State = () => {
+//     let value = 0;
+//     const handleButtonClick = () => {
+//         value++;
+//         console.log(value);
+// };
+// let array=useState();
+// console.log(array);
+const [count, setCount] = useState(5);
+console.log("Parent Component rendered");
+const handleButtonClick = () => {
+    setCount (() => count +1);
+}
+return (
+    <>
+    <div className="main-div">
+    <h1>{count}</h1>
+    {/* <button onClick={handleButtonClick}>Increment</button> */}
+    <button onClick={handleButtonClick}>Increment</button>
+    </div> 
+    <ChildComponent count={count}/>
+    </>
+);
+};
+
+function ChildComponent({count}) {
+    console.log("Child Component rendered");
+    return (
+        <div className="main-div">
+            <h2>Child Component - {count}</h2>
+        </div>
+        );
+}
